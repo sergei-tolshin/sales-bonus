fe7f88b4ef3a41b12d6ffbfeddda06705fd8ac66
Sergei Tolshin <tolshin.sergei@yandex.ru>
2026-08-18 22:43:09 +0500

    Проверка данных и опций

--- src/main.js
+++ src/main.js
@@ -6,9 +6,9 @@
  */
 function calculateSimpleRevenue(purchase, _product) {
    const { discount, sale_price, quantity } = purchase;
-   const discount = 1 - discount / 100;
+   const discountFactor = 1 - discount / 100;
 
-   return sale_price * quantity * discount;
+   return sale_price * quantity * discountFactor;
 }
 
 /**
@@ -28,15 +28,57 @@ function calculateBonusByProfit(index, total, seller) {
 }
 
 /**
+ * Валидация входных данных
+ * @param data
+ */
+function validateData(data) {
+    if (!data) {
+        throw new Error("Данные не переданы");
+    }
+
+    const collections = ["sellers", "products", "purchase_records"];
+    for (const collection of collections) {
+        if (!Array.isArray(data[collection])) {
+            throw new Error(`Коллекция '${collection}' должна быть массивом`);
+        }
+        if (data[collection].length === 0) {
+            throw new Error(`Коллекция '${collection}' не может быть пустой`);
+        }
+    }
+}
+
+/**
+ * Валидация опций
+ * @param options
+ */
+function validateOptions(options) {
+    if (
+        !options
+        || typeof options !== "object"
+        || Array.isArray(options)
+    ) {
+        throw new Error("Опции должны быть объектом");
+    }
+
+    const params = ["calculateRevenue", "calculateBonus"];
+    for (const param of params) {
+        if (typeof options[param] !== "function") {
+            throw new Error(`Параметр '${param}' должен быть функцией`);
+        }
+    }
+}
+
+/**
  * Функция для анализа данных продаж
  * @param data
  * @param options
  * @returns {{revenue, top_products, bonus, name, sales_count, profit, seller_id}[]}
  */
 function analyzeSalesData(data, options) {
-    // @TODO: Проверка входных данных
+    validateData(data);
+    validateOptions(options); 
 
-    // @TODO: Проверка наличия опций
+    const { calculateRevenue, calculateBonus } = options;
 
     // @TODO: Подготовка промежуточных данных для сбора статистики
 
